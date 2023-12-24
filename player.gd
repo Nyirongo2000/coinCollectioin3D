fb62a1f15f4f64068fc2f20315c90da7ea22c888
oliver nyirongo <bed-vom-21-19@unima.ac.mw>
2023-12-24 22:10:41 +0200

    qwerty

--- player.gd
+++ player.gd
@@ -1,34 +1,29 @@
 extends CharacterBody3D
 
-
-
 @export var SPEED = 16.0
-@export var jump_strength  =20
-#@export var fall_acceleration = 75
+@export var jump_strength = 20
 @onready var camera_mount_node_3d = $"Camera_Mount Node3D"
 @onready var animation_player = $Node3D_pivot_visuals/gobot/AnimationPlayer
 #for rotation
-@export var sensing_horizontal_mvmet =0.4
-@export var sensing_vetical_mvmet =0.4
+@export var sensing_horizontal_mvmet = 0.4
+@export var sensing_vetical_mvmet = 0.4
 var target_velocity = Vector3.ZERO
 @onready var node_3d_pivot_visuals = $Node3D_pivot_visuals
-
 
 const JUMP_VELOCITY = 9.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-#var gravity = 0
-#when player is ready
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
 
-#getting mouse motion
-#
+# when player is ready
+func _ready():
+	animation_player.play("Idle")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# getting mouse motion
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sensing_horizontal_mvmet))
-		#node_3d_pivot_visuals.rotate_y(deg_to_rad(event.relative.x * sensing_horizontal_mvmet))
 		camera_mount_node_3d.rotate_x(deg_to_rad(-event.relative.y * sensing_vetical_mvmet))
 
 func _physics_process(delta):
@@ -40,26 +35,43 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animation_player.play("Jump")
-			
-	
+	elif not is_on_floor() and velocity.y < 0:
+		animation_player.play('Fall')
+	#elif is_on_floor() and velocity == Vector3.ZERO:
+		#animation_player.play('Idle')
+		#animation_player.play("Jump")
+		#print("Current Animation:", animation_player.current_animation)
+
+	# Handle falling animation.
+	#if not is_on_floor() and velocity.y < -0.1 and animation_player.current_animation != "Fall":
+		#animation_player.play("Fall")
+		#print("Current Animation:", animation_player.current_animation)
+
+
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		#walking animation
+		# walking animation
 		if animation_player.current_animation != "Walk":
 			animation_player.play("Walk")
-		#to rotate the pivot in walking direction respectivly 
-		#negate direction fix
+			print("Current Animation:", animation_player.current_animation)
+
+		# to rotate the pivot in walking direction respectively
+		# negate direction fix
 		node_3d_pivot_visuals.look_at(position - direction, Vector3.UP)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		#idle animation
+		# idle animation
 		if animation_player.current_animation != "Idle":
 			animation_player.play("Idle")
-		
+			print("Current Animation:", animation_player.current_animation)
+
+
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
