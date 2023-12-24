1cdb8acd7ee3cd6dc760f2a2a990f19d4c2ef60b
oliver nyirongo <bed-vom-21-19@unima.ac.mw>
2023-12-25 01:29:23 +0200

    movement anime

---
+++ beetle.gd
@@ -0,0 +1,65 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+var attackPlayer = false
+var chasePlayer = false
+@onready var mesh = get_node("beetle_bot_fused")
+@onready var animation_player = $beetle_bot_fused/AnimationPlayer
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var direction
+var health
+
+func _physics_process(delta):
+	direction = Vector2.ZERO
+#	incase, the beetle is trying to fall off from something
+#	if not is_on_floor():
+#		velocity = gravity * delta
+	
+	var player = get_node("../../character/Player")
+	if player:
+		direction = (player.position - position).normalized()
+	if chasePlayer:
+		if direction:
+			mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta*10)
+		animation_player.play("walk")
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+			
+	elif attackPlayer:
+		if direction:
+			mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta*10)
+			animation_player.play("headbutt")
+			velocity.x = 0
+			velocity.z = 0
+	else:
+		animation_player.play("idle")
+		velocity.x = 0
+		velocity.z = 0
+	move_and_slide()
+
+func _on_attacking_area_body_entered(body):
+	if "Player" in body.name:
+		attackPlayer = true
+		#no need to chase the player when the player is being attacked
+		chasePlayer = false
+
+
+func _on_chasing_area_body_entered(body):
+	if "Player" in body.name:
+		chasePlayer = true
+
+
+func _on_attacking_area_body_exited(body):
+	if "Player" in body.name:
+		attackPlayer =false
+		chasePlayer = true
+
+
+func _on_chasing_area_body_exited(body):
+	if "Player" in body.name:
+		chasePlayer = false
--- player.gd
+++ player.gd
@@ -27,6 +27,12 @@ func _input(event):
 		camera_mount_node_3d.rotate_x(deg_to_rad(-event.relative.y * sensing_vetical_mvmet))
 
 func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -35,8 +41,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animation_player.play("Jump")
-	elif not is_on_floor() and velocity.y < 0:
-		animation_player.play('Fall')
+#	if not is_on_floor() and velocity.y < 0:
+#		animation_player.play('Fall')
+
+	if is_on_floor() and direction:
+		animation_player.play("Walk")
+	else:
+		animation_player.play("Idle")
+#
+	if not is_on_floor() and velocity.y >0:
+		animation_player.play("Jump")
 	#elif is_on_floor() and velocity == Vector3.ZERO:
 		#animation_player.play('Idle')
 		#animation_player.play("Jump")
@@ -49,16 +63,12 @@ func _physics_process(delta):
 
 
 		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
 		# walking animation
-		if animation_player.current_animation != "Walk":
-			animation_player.play("Walk")
-			print("Current Animation:", animation_player.current_animation)
+#		if animation_player.current_animation != "Walk":
+#			animation_player.play("Walk")
+#			print("Current Animation:", animation_player.current_animation)
 
 		# to rotate the pivot in walking direction respectively
 		# negate direction fix
@@ -67,9 +77,9 @@ func _physics_process(delta):
 		velocity.z = direction.z * SPEED
 	else:
 		# idle animation
-		if animation_player.current_animation != "Idle":
-			animation_player.play("Idle")
-			print("Current Animation:", animation_player.current_animation)
+#		if animation_player.current_animation != "Idle":
+#		animation_player.play("Idle")
+#		print("Current Animation:", animation_player.current_animation)
 
 
 		velocity.x = move_toward(velocity.x, 0, SPEED)
